71a9cdda62741f39d59390086e50c8ac08b6d6b1
Manish Budhathoki <64700505+mbudhathoki200@users.noreply.github.com>
2023-06-13 06:29:52 +0545

    ✅Tabbed Component

--- script.js
+++ script.js
@@ -69,9 +69,21 @@ const tabs=document.querySelectorAll('.operations__tab');
 const tabsContainer=document.querySelector('.operations__tab-container');
 const tabsContent=document.querySelectorAll('.operations__content');
 tabsContainer.addEventListener('click',function(e){
-  if(e.target.classList.contains('operations__tab')){
-    console.log("GOT LINK");
-  }
+  // if(e.target.classList.contains('operations__tab')){
+  //   console.log("GOT LINK");
+  // }
+  const clicked=e.target.closest('.operations__tab');
+  console.log(clicked);
+  //Guard Early
+  if(!clicked) return;
+  //Remove Active classes
+  tabs.forEach(t=>t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c=>c.classList.remove('operations__content--active'));
+  //Active tab
+  clicked.classList.add('operations__tab--active');
+  //Active Content Area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
 })
 
 
